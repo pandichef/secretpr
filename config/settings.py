117665fc2e13c6d36751a68d6c4c53bf048d81c1
pandichef <rashtyda@gmail.com>
2023-10-19 15:32:46 -0500

    STATIC_ROOT

--- config/settings.py
+++ config/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+if os.name == "posix":
+    STATIC_ROOT = "/home/secretpr/static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
